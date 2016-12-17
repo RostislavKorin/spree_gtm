9e1c0e012beb51fc5b782ab927ce660ecda86d27
rost <rostislav.korin@gmail.com>
2016-12-17 21:08:41 +0200

    Create controller and model

---
+++ app/controllers/spree/admin/gtms_controller.rb
@@ -0,0 +1,11 @@
+class Spree::Admin::GtmsController < Spree::Admin::ResourceController
+
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
---
+++ app/models/spree/gtm.rb
@@ -0,0 +1,3 @@
+class Spree::Gtm < ActiveRecord::Base
+
+end
