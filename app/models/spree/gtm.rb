a9fd97fccdfcdab780358c765991ef5c5100495a
rost <rostislav.korin@gmail.com>
2016-12-19 10:02:02 +0200

    Update model

--- app/models/spree/gtm.rb
+++ app/models/spree/gtm.rb
@@ -1,9 +1,12 @@
 class Spree::Gtm < ActiveRecord::Base
   validates :gtm_accountid, presence: true
 
+  def self.activated?
+    first.present?
+  end
 
   def self.gtm_accountid
-      first.gtm_accountid
-    end
+    first.gtm_accountid
+  end
 
 end
